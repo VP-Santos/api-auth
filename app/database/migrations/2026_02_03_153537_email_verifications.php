6cafa65abf5e6e7403e42a837eadb760d89ff9ce
Vitor Pereira dos Santos <vitor.santos@satcompany.com.br>
2026-02-07 13:42:35 -0300

    fix: alteration migrations

--- app/database/migrations/2026_02_03_153537_email_verifications.php
+++ app/database/migrations/2026_02_03_153537_email_verifications.php
@@ -13,16 +13,11 @@ return new class extends Migration
     {
         Schema::create('email_verifications', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-            ->references('id')
-            ->on('users')
-            ->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('token')->unique();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
-        
     }
 
     /**
