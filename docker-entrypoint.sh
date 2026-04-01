058c75d73cc93079f2f804f76bb6af319602883d
vitor <vitor.psantos1999@gmail.com>
2026-03-31 22:01:25 -0300

    fix docker compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,9 +16,16 @@ if [ ! -d "$APP_DIR/vendor" ]; then
     composer install --no-interaction --optimize-autoloader
 fi
 
+# Gera a APP_KEY (se ainda não existir)
+if ! grep -q "^APP_KEY=" "$APP_DIR/.env" || grep -q "^APP_KEY=$" "$APP_DIR/.env"; then
+    echo "Gerando APP_KEY..."
+    php artisan key:generate
+fi
+
 echo "Ajustando permissões..."
 [ -d "$APP_DIR/storage" ] && chmod -R 777 $APP_DIR/storage
 [ -d "$APP_DIR/bootstrap/cache" ] && chmod -R 777 $APP_DIR/bootstrap/cache
+
 
 echo "Iniciando PHP-FPM..."
 exec "$@"
